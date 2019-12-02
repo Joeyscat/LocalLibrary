ffa3f12c0c2198e1f3734ce088547064733752f4
Joeyscat <zhouyu.fun@gmail.com>
2019-12-02 20:35:20 +0800

    初始化项目

---
+++ controllers/bookController.js
@@ -0,0 +1,29 @@
+const Book = require('../models/book')
+
+exports.index = (req, res) => {
+  res.send('未实现：藏书编目主页')
+}
+exports.book_create_get = (req, res) => {
+  res.send('未实现：添加新的藏书')
+}
+exports.book_create_post = (req, res) => {
+  res.send('未实现：添加新的藏书')
+}
+exports.book_delete_get = (req, res) => {
+  res.send('未实现：删除藏书')
+}
+exports.book_delete_post = (req, res) => {
+  res.send('未实现：删除藏书')
+}
+exports.book_update_get = (req, res) => {
+  res.send('未实现：更新藏书')
+}
+exports.book_update_post = (req, res) => {
+  res.send('未实现：更新藏书')
+}
+exports.book_detail = (req, res) => {
+  res.send('未实现：请求藏书')
+}
+exports.book_list = (req, res) => {
+  res.send('未实现：请求藏书列表')
+}
