afc5edbd2a4da18b298fd33e8d4029eac9c2eca9
Joeyscat <zhouyu.fun@gmail.com>
2019-12-07 00:56:08 +0800

    使用Promise优化代码

--- src/controllers/api/genreController.js
+++ src/controllers/api/genreController.js
@@ -1,52 +1,68 @@
 const service = require('../../service/genreService')
-const { validationResult } = require('express-validator');
-
+const { validationResult } = require('express-validator')
 
 exports.create = (req, res, next) => {
   const errors = validationResult(req)
   console.error(errors)
 
   if (!errors.isEmpty()) {
-    res.json({ errors: errors.array() });
-  } else {
-    const resolve = genre => {
-      service.detail(genre._id, (genreDetail) => { return res.json(genreDetail) }, next)
-    }
-    service.create(req, resolve, next)
+    return res.json({ errors: errors.array() })
   }
+  service
+    .create(req)
+    .then(genre => {
+      service
+        .detail(genre._id)
+        .then(genreDetail => {
+          return res.json(genreDetail)
+        })
+        .catch(err => next(err))
+    })
+    .catch(err => next(err))
 }
 
 exports.delete = (req, res, next) => {
-
-  service.delete(req.params.id, (result) => {
-    return res.json(result)
-  }, next)
+  service
+    .delete(req.params.id)
+    .then(result => {
+      return res.json(result)
+    })
+    .catch(err => next(err))
 }
 
 exports.update = (req, res, next) => {
   const errors = validationResult(req)
-  console.error(errors)
 
   if (!errors.isEmpty()) {
-    return res.json({ errors: errors.array() });
-  } else {
-    const resolve = genre => {
-      service.detail(genre._id, (genreDetail) => { return res.json(genreDetail) }, next)
-    }
-    service.update(req, resolve, next)
+    return res.json({ errors: errors.array() })
   }
+  service
+    .update(req)
+    .then(genre => {
+      service
+        .detail(genre._id)
+        .then(genreDetail => {
+          return res.json(genreDetail)
+        })
+        .catch(err => next(err))
+    })
+    .catch(err => next(err))
 }
 
 exports.detail = (req, res, next) => {
-
-  service.detail(req.params.id, (genre) => {
-    return res.json(genre)
-  }, next)
+  service
+    .detail(req.params.id)
+    .then(genre => {
+      return res.json(genre)
+    })
+    .catch(err => next(err))
 }
 
 exports.list = (req, res, next) => {
-
-  service.list((genres) => {
-    return res.json(genres)
-  }, next)
+  service
+    .list()
+    .then(result => {
+      return res.json(result)
+    })
+    .catch(err => next(err))
 }
--- src/service/genreService.js
+++ src/service/genreService.js
@@ -1,87 +1,89 @@
 const Genre = require('../models/genre')
 const mongoose = require('mongoose')
 
-exports.create = (req, resolve, reject) => {
-  const name = req.body.name;
-  Genre.findOne({ name: name }, (err, genre) => {
-    if (err) {
-      return reject(err)
-    } else {
-      if (genre == null) {
-        const genre = new Genre({
-          name: name
-        })
-
-        genre.save(function (err) {
-          if (err) {
-            return reject(err)
-          }
-          resolve(genre)
-        })
-      } else {
+exports.create = req => {
+  const name = req.body.name
+  return new Promise((resolve, reject) => {
+    Genre.findOne({ name: name }, (err, genre) => {
+      if (err) {
+        return reject(err)
+      }
+      if (genre != null) {
         return reject({ msg: '已存在该类型-' + name })
       }
-    }
+      genre = new Genre({
+        name: name
+      })
+
+      genre.save(function(err) {
+        if (err) {
+          return reject(err)
+        }
+        resolve(genre)
+      })
+    })
   })
 }
 
-exports.delete = (id, resolve, reject) => {
-  id = mongoose.Types.ObjectId(id)
-  Genre.findById(id, (err, genre) => {
-    if (err) {
-      return reject(err)
-    }
-    if (genre != null) {
+exports.delete = id => {
+  // id = mongoose.Types.ObjectId(id)
+  return new Promise((resolve, reject) => {
+    Genre.findById(id, (err, genre) => {
+      if (err) {
+        return reject(err)
+      }
+      if (genre == null) {
+        return reject({ msg: '找不到该类型' })
+      }
       Genre.findByIdAndRemove(id, err => {
         if (err) {
           return reject(err)
         }
         return resolve({ id })
       })
-    } else {
-      reject({ msg: '找不到该类型' })
-    }
+    })
   })
 }
 
-exports.update = (req, resolve, reject) => {
-  console.log(req.body)
-  id = mongoose.Types.ObjectId(req.body._id)
-  const genre = new Genre({
-    _id: id,
-    name: req.body.name
-  })
-  Genre.findByIdAndUpdate(id, genre, {}, function (err, modifiedgenre) {
-    if (err) {
-      return reject(err)
-    }
-    if (modifiedgenre == null) {
-      return reject({ msg: '找不到该类型' })
-    } else {
+exports.update = req => {
+  return new Promise((resolve, reject) => {
+    id = mongoose.Types.ObjectId(req.body._id)
+    const genre = new Genre({
+      _id: id,
+      name: req.body.name
+    })
+    // TODO 不可修改为已存在的类型
+    Genre.findByIdAndUpdate(id, genre, {}, function(err, modifiedgenre) {
+      if (err) {
+        return reject(err)
+      }
+      if (modifiedgenre == null) {
+        return reject({ msg: '找不到该类型' })
+      }
       resolve(modifiedgenre)
-    }
+    })
   })
 }
 
-exports.detail = (id, resolve, reject) => {
-  id = mongoose.Types.ObjectId(id)
-  Genre.findById(id, '-__v')
-    .exec((err, genre) => {
+exports.detail = id => {
+  // id = mongoose.Types.ObjectId(id)
+  return new Promise((resolve, reject) => {
+    Genre.findById(id, '-__v').exec((err, genre) => {
       if (err) {
         return reject(err)
-      } else {
-        resolve(genre)
       }
+      resolve(genre)
     })
+  })
 }
 
-exports.list = (resolve, reject) => {
-  Genre.find({}, '-__v')
-    .exec((err, genres) => {
+exports.list = () => {
+  return new Promise((resolve, reject) => {
+    Genre.find({}, '-__v').exec((err, genres) => {
       if (err) {
         return reject(err)
-      } else {
-        resolve(genres)
       }
+      resolve(genres)
     })
+  })
 }
